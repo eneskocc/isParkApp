a68d7ef460478214de464537b5e2e71c0cc53f48
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-22 18:08:27 +0300

    saga park id func yazıldı

--- isPark/app/redux/sagas.js
+++ isPark/app/redux/sagas.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 function* handler() {
     yield takeLatest($.GET_PARK, FUNC_GET_PARK);
- 
+    yield takeLatest($.GET_PARK_ID, FUNC_GET_PARK_ID);
 
 }
 
@@ -16,7 +16,7 @@ function* FUNC_GET_PARK(){
         yield put ({
             type:$.GET_PARK_REQUEST
         })
-        const response = yield axios.post(apiUrl)
+        const response = yield axios.post(apiUrl+'/Park')
        
         if(response.data.status==0){
             yield put({
@@ -42,6 +42,38 @@ function* FUNC_GET_PARK(){
         })
     }
 }
+
+function* FUNC_GET_PARK_ID(action){
+    try {
+        yield put ({
+            type:$.GET_PARK_ID_REQUEST
+        })
+        const response = yield axios.get(apiUrl+'/ParkDetay?id='+action.id)
+        if(response.data.status==0){
+            yield put({
+                type:$.GET_PARK_ID_REQUEST_FAILURE,
+                ERROR:response.data
+            })
+        }
+        else{
+            yield put({
+                type:$.GET_PARK_ID_REQUEST_SUCCESS,
+                PARK_ID:response.data
+            })
+        }
+    } catch (error) {
+        yield put({
+            type:$.GET_PARK_ID_REQUEST_FAILURE,
+            ERROR:error
+        })
+    }
+    finally{
+        yield put({
+            type:$.GET_PARK_ID_REQUEST_END
+        })
+    }
+}
+
 
 
 
