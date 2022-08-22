9627898b2431ed6bb45b3c9d2479d9bda2eb096b
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-22 18:08:31 +0300

    Update index.js

--- isPark/app/screens/Home/index.js
+++ isPark/app/screens/Home/index.js
@@ -6,6 +6,7 @@ import {
   FlatList,
   Image,
   ScrollView,
+  ActivityIndicator,
 } from 'react-native';
 import React, {useState, useEffect, componentDidMount, Component} from 'react';
 import MapView from 'react-native-map-clustering';
@@ -24,22 +25,16 @@ Geolocation.getCurrentPosition(info => {
     longitudeDelta: 1,
   };
 });
-const Item = ({item}) => (
-  <View
-    key={item.parkID}
-    style={{
-      backgroundColor: colors.green,
-      width: 200,
-      height: 100,
-      marginHorizontal: 10,
-      borderRadius: 10,
-    }}>
-    <Text>{item.parkName}</Text>
-  </View>
-);
+
 class Home extends Component {
   componentDidMount() {
     this.props.getData();
+    this.props.getDataId(780);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.GET_PARK_REQUEST_STATUS == 2) {
+      console.log('aaa' + this.props.PARK_ID.locationName);
+    }
   }
 
   render() {
@@ -61,6 +56,22 @@ class Home extends Component {
       return markers;
     };
 
+    if (this.props.GET_PARK_ID_REQUEST_STATUS == 2) {
+      return (
+        <View
+          style={{
+            backgroundColor: colors.green,
+            width: 200,
+            height: 100,
+            marginHorizontal: 10,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>tem.parkName</Text>
+        </View>
+      );
+    }
     return (
       <View style={{width: '150%'}}>
         <MapView
@@ -76,27 +87,73 @@ class Home extends Component {
             zIndex: 5,
             height: 100,
             paddingBottom: 100,
-            marginRight: 200,
+            
           }}>
-           <FlatList
-        data={this.props.PARK}
-        renderItem={Item}
-        keyExtractor={item => item.id}
-      />
+          {this.props.GET_PARK_ID_REQUEST_ERROR ? (
+            <Text
+              style={{
+                color: colors.red,
+                fontFamily: 'Poppins-Bold',
+                textAlign: 'center',
+                alignSelf: 'center',
+                marginTop: '50%',
+                fontSize: 20,
+              }}>
+              eneee
+            </Text>
+          ) : (
+            this.props.FAVORITE.map(item => (
+              <View
+                style={{
+                  backgroundColor: colors.green,
+                  width: '100%',
+                  height: 100,
+                  marginHorizontal: 10,
+                  borderRadius: 10,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text>{item.parkName}</Text>
+                <Text>{item.workHours}</Text>
+                <Text>{item.address}</Text>
+                <Text>{item.parkType}</Text>
+              </View>
+            ))
+          )}
         </ScrollView>
       </View>
     );
   }
 }
 const mapStateToProps = (state, props) => {
-  const {PARK} = state.default;
-  return {PARK};
+  const {
+    PARK,
+    GET_PARK_REQUEST_STATUS,
+    PARK_ID,
+    FAVORITE,
+    GET_PARK_ID_REQUEST_STATUS,
+    GET_PARK_ID_REQUEST_ERROR,
+  } = state.default;
+  return {
+    PARK,
+    GET_PARK_REQUEST_STATUS,
+    PARK_ID,
+    FAVORITE,
+    GET_PARK_ID_REQUEST_STATUS,
+    GET_PARK_ID_REQUEST_ERROR,
+  };
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
   getData: () => {
     dispatch({
       type: $.GET_PARK,
+    });
+  },
+  getDataId: id => {
+    dispatch({
+      type: $.GET_PARK_ID,
+      id,
     });
   },
 });
