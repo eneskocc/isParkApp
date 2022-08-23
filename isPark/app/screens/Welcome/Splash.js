70c75aef1efd284a771a4fe712115a7dc7489fc9
Enes koç <32553624+eneskocc@users.noreply.github.com>
2022-08-23 15:04:33 +0300

    Create Splash.js

---
+++ isPark/app/screens/Welcome/Splash.js
@@ -0,0 +1,28 @@
+import {Text, View, SafeAreaView, Image} from 'react-native';
+import React, {Component,componentDidMount} from 'react';
+
+export class Splash extends Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.navigation.navigate("Home")
+        }, 1000)
+    }
+  render() {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}>
+        <Image
+          style={{width: '100%', height: 200, resizeMode: 'contain'}}
+          source={require('../../assets/images/home/logo.png')}
+        />
+      </SafeAreaView>
+    );
+  }
+}
+
+export default Splash;
